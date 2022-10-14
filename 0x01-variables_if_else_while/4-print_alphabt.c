c02a4dea89ee16396ab65be6b0bf58009fe0e83d
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-14 12:06:13 -0700

    gg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * 	* main - enter the letters and provide with answers
+ *
+ * 	 * Return: 0 if succesfull
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low ='a'; low <= 'z' ; low++)
+	{
+		if (low != e && low !=q)
+			putchar(low);
+	}
+	printf("\n");
+
+	return (0);
+}
