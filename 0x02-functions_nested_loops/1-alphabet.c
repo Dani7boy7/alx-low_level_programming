ac62fd060c58e61ebc613c24375755130fcc26dd
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-18 05:45:03 -0700

    1.alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *	* print_alphabet - print lowecase alphabet
+ *
+ *	* Return: 0 when succesful
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar("\n");
+
+}
