8978550cbbdd92ba8684dcd832926615d0bdede5
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-18 06:35:19 -0700

    third

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *	* _islower - checks lowercase letters
+ *	* @c: is the letter to be checked
+ *	* Return: 1 if it is lowercase and 0 if not
+ *
+ */
+
+int _islower(int c)
+
+{
+	return (c >= 'a' && c <= 'z');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0.putchar
 1.alphabet
 alphabet x10
+3.is lower
