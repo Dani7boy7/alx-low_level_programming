8455b29b1b35a42c2abebf0130f915222e6ffc11
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-19 06:17:35 -0700

    second

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *	* main - creates an infinite loop
+ *	* Return: 0 when succesful
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *	putchr(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
