c20e09c23d3041597e3154612ad6048b8e3e4d6b
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-24 05:21:09 -0700

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *	* reset_to_98 - it takes a parameter to integer as a paramete
+ *	* and updates it to a point
+ *	* @n: the input
+ *	* Return: n
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
