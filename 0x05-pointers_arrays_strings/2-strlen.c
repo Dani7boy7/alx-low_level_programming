ebd211debceb127c54df55eb6d7b520c1a87861f
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-24 06:03:57 -0700

    third

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *	* _strlen - gives the length of string
+ *	* @s: the string
+ *	* Return: the length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int logi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
