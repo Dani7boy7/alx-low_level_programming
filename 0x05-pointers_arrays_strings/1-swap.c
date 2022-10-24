e4b072df8d40f7213c75eac20a2f5527b30faba5
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-24 05:37:00 -0700

    second

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * 	* swap_int - swaps both integers
+ * 	* @a: the first number to be swaped
+ * 	* @b: the second integer to be swaped
+ *
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
