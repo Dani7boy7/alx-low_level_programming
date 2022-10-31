b1552cb27658cc75d1f6128ca4a72d1e35b2142f
Dani7boy7 <www.danisolo7@gmail.com>
2022-10-31 08:41:58 -0700

    fifth

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *	* _STRPBRK - ENTRY POINT
+ *	* @s: first input
+ *	* @accept: second input
+ *	* Return: 0 when succesful
+ *
+ */
+ 
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
