c8eb3fab9755e9ef6a8622ca36bc8cea46d8c0be
Dani7boy7 <www.danisolo7@gmail.com>
2022-11-02 06:16:15 -0700

    fix first

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -4,8 +4,9 @@
 /**
  * 	* _putchar - gives the character c to stdout
  * 	* @c: the character to be printed
- * 	* Return: 1 when succesful -1 when not
+ * 	* Return: 1 when succesful -1 on error and errno is set apropriate
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
