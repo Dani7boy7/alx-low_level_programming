d2d3fcf909dfd93ae692cd004db59af2a7c333a7
Dani7boy7 <www.danisolo7@gmail.com>
2022-11-07 07:27:42 -0800

    seven

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
