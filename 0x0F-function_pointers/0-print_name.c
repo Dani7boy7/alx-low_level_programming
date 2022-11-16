5e5f47734b3df9c2e6e35d0502f3d15fb4c94cc0
Dani7boy7 <www.danisolo7@gmail.com>
2022-11-16 05:40:36 -0800

    first

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *	* print_name - print the name
+ *	* @name: string to add
+ *	* @f: change pointer to function
+ *	* Return: 0 when succesful
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f ++ NULL)
+		return;
+
+	f(name);
+}
