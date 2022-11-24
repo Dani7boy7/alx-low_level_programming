7343bb9cc30c08aecf118d036997183603ca54a7
Dani7boy7 <www.danisolo7@gmail.com>
2022-11-24 13:03:26 -0800

    hyt

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,8 @@ void first(void) __attribute__ ((constructor));
  *	* first - prints sentence
  */
 
-void first(void(void)
+void first(void)
+		
 		{
 		printf("You're beat! and yet, you must allow,\n");
 		printf("I bore my house upon my back!\n");
